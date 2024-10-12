2cd9bf038ecddd8a1662eeb482ad68ccf58ff36a
Freddye Pontes Nunes <freddye.pontes@outlook.com>
2024-10-12 19:47:22 -0300

    aaaaddddss

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -13,6 +13,7 @@ const FinanceControl = () => {
           <Link to="/" className="link">
             <button type="button" className="btn btn1 btn-sep" id="button1">
               <i className="fa-solid fa-house"></i>Home
+              <div className="Indicador2"></div>
             </button>
           </Link>
           <Link to="/ContasRec" className="link">

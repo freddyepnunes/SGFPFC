0c0415f7e07d04d61ff29db1e2786b280eeb7ca1
Raphael-Ninomiya <100524008+Raphael-Ninomiya@users.noreply.github.com>
2024-11-02 14:54:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom"; // Importa os componentes necessários
-import SGF from "./SGF/SGF.jsx";
-import ContasRec from "./ContasRec/ContasRec.jsx";
-import ContasPag from "./ContasPag/ContasPag.jsx";
-import Perfil from "./Perfil/Perfil.jsx";
-import Login from "./Login/Login.jsx";
-import Cadastro from "./Login/Cadastro.jsx";
-import BemVindo from "./BemVindo/BemVindo.jsx";
+import React from "react"; // Importa o React para criar o componente
+import { Route, Routes } from "react-router-dom"; // Importa as funcionalidades de roteamento para a definição de rotas de navegação
+import SGF from "./SGF/SGF.jsx"; // Página principal (Sistema de Gestão Financeira - SGF)
+import ContasRec from "./ContasRec/ContasRec.jsx"; // Página de Contas a Receber
+import ContasPag from "./ContasPag/ContasPag.jsx"; // Página Contas a Pagar
+import Login from "./Login/Login.jsx"; // Página de Login
+import Cadastro from "./Login/Cadastro.jsx"; // Página de Cadastro
+import BemVindo from "./BemVindo/BemVindo.jsx"; // Página de Boas-Vindas
 
+// Função principal do aplicativo que define as rotas de cada página
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<BemVindo />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Home" element={<SGF />} />
-      <Route path="/ContasRec" element={<ContasRec />} />
-      <Route path="/ContasPag" element={<ContasPag />} />
-      <Route path="/Perfil" element={<Perfil />} />
-      <Route path="/CadastroLogin" element={<Cadastro />} />
+      {/* Define o caminho (URL) e o componente exibido em cada rota */}
+      <Route path="/" element={<BemVindo />} /> {/* Página inicial de boas-vindas */}
+      <Route path="/Login" element={<Login />} /> {/* Página de Login */}
+      <Route path="/Home" element={<SGF />} /> {/* Página principal do sistema (SGF) */}
+      <Route path="/ContasRec" element={<ContasRec />} /> {/* Página de Contas a Receber */}
+      <Route path="/ContasPag" element={<ContasPag />} /> {/* Página de Contas a Pagar */}
+      <Route path="/CadastroLogin" element={<Cadastro />} /> {/* Página de Cadastro */}
     </Routes>
   );
 }
 
-export default App;
+export default App; // Exporta o componente App para ser usado em outros arquivos

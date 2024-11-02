12ac6e2d457e73e295916551894e8c1b62b87860
Raphael-Ninomiya <100524008+Raphael-Ninomiya@users.noreply.github.com>
2024-11-02 16:18:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
+// Importa a função defineConfig do Vite, que é usada para configurar o projeto
 import { defineConfig } from "vite";
+// Importa o plugin do React para o Vite, permitindo suporte a JSX e outros recursos do React
 import react from "@vitejs/plugin-react";
 
+// Exporta a configuração do Vite como um objeto
 export default defineConfig({
-  plugins: [react()],
+  // Plugins a serem usados no projeto
+  plugins: [react()], // Adiciona o plugin do React
+
+  // Configurações do servidor de desenvolvimento
   server: {
+    // Configuração do proxy para redirecionar solicitações de API
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // URL do seu backend Node.js
-        changeOrigin: true,
-        secure: false,
+        target: "http://localhost:5000", // URL do seu backend Node.js (onde a API está rodando)
+        changeOrigin: true, // Altera a origem da solicitação para a URL de destino
+        secure: false, // Permite solicitações a servidores que não têm um certificado SSL válido
       },
     },
   },
